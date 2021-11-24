da5d9339a03cd7403ae131a02b3d8f1d2d866751
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-11-24 11:58:47 +0800

    Revert "local composer"
    This reverts commit ffd6aaa6ea03515e71134f114fe8afb520ca5a51.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'ae962c98aecd7b672955d5887028977664967a91',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'ae962c98aecd7b672955d5887028977664967a91',
             'dev_requirement' => false,
         ),
         'brick/math' => array(
