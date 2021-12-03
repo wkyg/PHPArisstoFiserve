f3fcf20645651a5f8889cfaad1217a74f01b268b
WK Yong-Arissto <39939811+wkyg@users.noreply.github.com>
2021-12-03 17:25:06 +0800

    tester

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+
+$apiInstance = new FirstData\FirstApi\Client\Api\AuthenticationApi(
+    // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+    // This is optional, `GuzzleHttp\Client` will be used as default.
+    new GuzzleHttp\Client()
+);
+$contentType = 'application/json'; // string | Content type.
+$clientRequestId = 'clientRequestId_example'; // string | A client-generated ID for request tracking and signature creation, unique per request.  This is also used for idempotency control. We recommend 128-bit UUID format.
+$apiKey = 'csn5gVMfGgXh1cnFtimlHQOH1zNERw7Q'; // string | Key given to merchant after boarding associating their requests with the appropriate app in Apigee.
+$timestamp = timestamp(); // int | Epoch timestamp in milliseconds in the request from a client system. Used for Message Signature generation and time limit (5 mins).
+$accessTokenRequest = new \FirstData\FirstApi\Client\Model\AccessTokenRequest(); // \FirstData\FirstApi\Client\Model\AccessTokenRequest | Access token request
+$messageSignature = 'messageSignature_example'; // string | Used to ensure the request has not been tampered with during transmission. The Message-Signature is the Base64 encoded HMAC hash (SHA256 algorithm with the API Secret as the key.) For more information, refer to the supporting documentation on the Developer Portal.
+
+try {
+    $result = $apiInstance->authenticationAccessTokensPost($contentType, $clientRequestId, $apiKey, $timestamp, $accessTokenRequest, $messageSignature);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling AuthenticationApi->authenticationAccessTokensPost: ', $e->getMessage(), PHP_EOL;
+}
+
+/*
     require_once(__DIR__ . '/vendor/autoload.php');
 
     //C:\xampp\htdocs\PHPArisstoFiserve\vendor\first-data\gateway\src\Configuration.php
@@ -60,4 +83,5 @@
     } catch (Exception $e) {  
         echo $e->getMessage();          
     }
+*/
 ?>
